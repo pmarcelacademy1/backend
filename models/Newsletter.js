a9fce1d71986407bfbeda994ec31d955495e1043
pmarcelacademy1 <pmarcelacademy@gmail.com>
2025-04-01 01:45:52 +0100

    newsletter model changed

--- models/Newsletter.js
+++ models/Newsletter.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose"
 
 
 const newsletterSchema = new mongoose.Schema({
-   user: {
-           type: mongoose.Schema.Types.ObjectId,
-           ref: "User",
-           required: true,
-         },
   email: { type: String, required: true, unique: true },
   subscribedAt: { type: Date, default: Date.now }
 });
